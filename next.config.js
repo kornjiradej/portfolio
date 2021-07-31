429a98c34414bb4b1b8d133077c202bd6d444f22
korn jiradej <kornji@jenosize.co>
2021-07-31 22:02:08 +0700

    [update] next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,52 @@
-module.exports = {
-  basePath: '/wallisconsultancy',
-  assetPrefix: '/wallisconsultancy/', 
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+const {
+  EMAIL_JS_SERVICE,
+  EMAIL_JS_TEMPLATE,
+  EMAIL_JS_USER,
+  CAPTCHA_SITE_KEY,
+  GTAG,
+  ON_GITHUB_PAGES,
+} = process.env;
+
+const basePath = ON_GITHUB_PAGES ? '/wallisconsultancy' : '';
+const assetPrefix = ON_GITHUB_PAGES ? '/wallisconsultancy/' : '';
+
+const env = {
+  EMAIL_JS_SERVICE,
+  EMAIL_JS_TEMPLATE,
+  EMAIL_JS_USER,
+  CAPTCHA_SITE_KEY,
+  GTAG,
+};
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+  }],
+  {
+    env,
+    basePath,
+    assetPrefix,
+    images: {
+      loader: "imgix",
+      path: "",
+    },
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        '/': { page: '/' }, 
+      }
+    },
+  },
+]);
