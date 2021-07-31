1a3e4fe28efd4ed4c24f4dfb981a89b6c71d35cd
korn jiradej <kornji@jenosize.co>
2021-07-31 18:10:14 +0700

    [update] yarn

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Link from "next/link"
 
 export default function Home() {
   return (
@@ -14,22 +14,15 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://github.com/kornjiradej">Korn Jiradej World!</a>
-          <Link href={`${process.env.BACKEND_URL}${href}`}>{href}</Link>
+          Welcome to <a href="https://github.com/kornjiradej">Korn Jiradej World!</a> 
         </h1>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Powered by{' '}
+        <span className={styles.logo}>
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </span>
       </footer>
     </div>
   )
